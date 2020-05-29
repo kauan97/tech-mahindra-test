429def2973726996a86e91062be877b7d2e758a1
Orlando Silva <orlandosilva@kpmg.com.br>
2020-05-29 15:58:51 -0300

    ADD Procfile

--- __tests__/integration/auth.spec.js
+++ __tests__/integration/auth.spec.js
@@ -15,59 +15,64 @@ describe('User authentication', () => {
 
     beforeEach(async () => {
         await db.truncate()
-    })
-
-	it('should be able to register a user', async () => {
-		const { nome, email, senha, telefones } = await factory.create('User')
-		const user = {
-			nome,
-			email: 'orlandokauan.barros@gmail.com',
-			senha,
-			telefones
-		}
-
-        const response = await request(app)
-			.post('/api/auth/register')
-			.send(user)
-
-		expect(response.status).toBe(201)
-		expect(response.body).toHaveProperty('_id')
-		expect(response.body).toHaveProperty('data_criacao')
-		expect(response.body).toHaveProperty('data_atualizacao')
-		expect(response.body).toHaveProperty('ultimo_login')
 	})
 
-	it('should not be able to allow user registration when an email already exists', async () => {
-		const { nome, email, senha, telefones } = await factory.create('User')
-		const user = {
-			nome,
-			email,
-			senha,
-			telefones
-		}
+	describe('Sign up', () => {
+		it('should be able to register a user', async () => {
+			const { nome, email, senha, telefones } = await factory.create('User')
+			const user = {
+				nome,
+				email: 'orlandokauan.barros@gmail.com',
+				senha,
+				telefones
+			}
 
-        const response = await request(app)
-			.post('/api/auth/register')
-			.send(user)
+			const response = await request(app)
+				.post('/api/auth/register')
+				.send(user)
 
-		expect(response.status).toBe(400)
-		expect(response.body.mensagem).toBe('E-mail já existente.')
-	})
-
-	it('should authenticate the user with valid credentials', async () => {
-		const senha = '123123'
-
-		const user = await factory.create("User", {
-			senha
+			expect(response.status).toBe(201)
+			expect(response.body).toHaveProperty('_id')
+			expect(response.body).toHaveProperty('data_criacao')
+			expect(response.body).toHaveProperty('data_atualizacao')
+			expect(response.body).toHaveProperty('ultimo_login')
 		})
 
-        const response = await request(app)
-			.post('/api/auth/authenticate')
-			.send({
-				email: user.email,
+		it('should not be able to allow user registration when an email already exists', async () => {
+			const { nome, email, senha, telefones } = await factory.create('User')
+			const user = {
+				nome,
+				email,
+				senha,
+				telefones
+			}
+
+			const response = await request(app)
+				.post('/api/auth/register')
+				.send(user)
+
+			expect(response.status).toBe(400)
+			expect(response.body.mensagem).toBe('E-mail já existente.')
+		})
+	})
+
+	describe('Sign in', () => {
+		it('should authenticate the user with valid credentials', async () => {
+			const senha = '123123'
+
+			const user = await factory.create("User", {
 				senha
 			})
 
-		expect(response.status).toBe(200)
+			const response = await request(app)
+				.post('/api/auth/authenticate')
+				.send({
+					email: user.email,
+					senha
+				})
+
+			expect(response.status).toBe(200)
+			expect(response.body).toHaveProperty('token')
+		})
 	})
 })
